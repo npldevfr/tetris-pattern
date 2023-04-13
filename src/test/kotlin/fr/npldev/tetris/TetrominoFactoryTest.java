54867cf0d8ae6da087241f390111f8646238fb99
Romain GAUTIER <romain.gautier@etu.univ-nantes.fr>
2023-04-13 19:59:19 +0200

    finalisation test

--- src/test/kotlin/fr/npldev/tetris/TetrominoFactoryTest.java
+++ src/test/kotlin/fr/npldev/tetris/TetrominoFactoryTest.java
@@ -1,5 +1,6 @@
 package fr.npldev.tetris;
 
+import fr.npldev.tetris.shapes.ITetromino;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.*;
 
@@ -7,29 +8,55 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
+/***
+ * Classe de Test sur les tetromino de manière générique
+ *
+ * @see TetrominoFactory
+ *      Puis chaque sur chaque tetromino, héritant de la classe Tetromino
+ * @see Tetromino
+ * @version 1.0.0
+ */
 @DisplayName("Tetronimo Factory Test")
 public class TetrominoFactoryTest {
 
-    private static Tetromino tetromino ;
+    private static Tetromino tetromino;
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
         tetromino = TetrominoFactory.INSTANCE.createRandomTetromino();
     }
 
-    @TestFactory
-    public Iterable<DynamicTest> multiple_tests(){
-        ArrayList<Color> colors = new ArrayList(Arrays.asList(Color.YELLOW, Color.BLUE, Color.YELLOW, Color.CYAN, Color.PINK, Color.GREEN, Color.RED));
-        Tetromino temp = tetromino;
-        for(int i=0;i<3;i ++) tetromino.rotate();
-        return Arrays.asList(
-                DynamicTest.dynamicTest("Is subClass of Tetronimo ?",
-                        () -> Assertions.assertTrue(Tetromino.class.isAssignableFrom(tetromino.getClass()))),
-                DynamicTest.dynamicTest("Has a color ?",
-                        () -> Assertions.assertTrue(colors.contains(tetromino.getColor()))),
-                DynamicTest.dynamicTest("4 rotates equals initial position",
-                        () ->  Assertions.assertArrayEquals(tetromino.getShape(),temp.getShape()))
-
-        );
+    @Test
+    public void isSubClassTetronimo() {
+        Assertions.assertTrue(Tetromino.class.isAssignableFrom(tetromino.getClass()));
+        Assertions.assertTrue(tetromino != null);
     }
+
+    @Test
+    public void hasColor() {
+        ArrayList<Color> colors = new ArrayList(
+                Arrays.asList(Color.YELLOW, Color.BLUE, Color.YELLOW, Color.CYAN, Color.PINK, Color.GREEN, Color.RED));
+        Assertions.assertTrue(colors.contains(tetromino.getColor()));
+    }
+
+    @Test
+    public void rotateToInitialPosition() {
+        Tetromino temp = tetromino;
+        for (int i = 0; i < 3; i++)
+            tetromino.rotate();
+        Assertions.assertArrayEquals(tetromino.getShape(), temp.getShape());
+    }
+
+    @Test
+    public void rotations() {
+        Tetromino temp = tetromino;
+        for (int i = 0; i < 3; i++) {
+            temp.rotate();
+            for (int j = 0; j < i - 1; j++) {
+                tetromino.rotate();
+            }
+        }
+        Assertions.assertArrayEquals(tetromino.getShape(), temp.getShape());
+    }
+
 }
